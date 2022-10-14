ff5c69aae9d5602dec7f7bec236fe05ce6b1e617
P A R I <37542814+Pari030@users.noreply.github.com>
2022-10-14 08:40:55 +0200

    Removed cringe
    Removed some stupid beginner things

--- src/main.py
+++ src/main.py
@@ -1,69 +1,38 @@
-"""
-    Main program for getting an ip by anydesk.
-"""
 import os
 import sys
 import wmi
 import psutil
-
-from requests import get
+import requests
 
 def get_ips() -> list:
-    """ Getting the list of ips of anydesk """
-
-    # Initializing the wmi constructor
     wmi_obj = wmi.WMI()
     ips = []
 
     # Iterating through all the running processes
     for process in wmi_obj.Win32_Process():
-
         try:
-            # Getting anydesk
             if 'anydesk' in process.Name.lower():
-
-                # Iterating process connections
                 for conn in psutil.Process(process.ProcessId).connections():
-
-                    # Getting only SYN SENT connections
-                    if conn.status in ('SYN_SENT', 'ESTABLISHED'):
-
-                        # Getting remote address ip and port
+                    if conn.status in ('SYN_SENT', 'ESTABLISHED'):  # Only connection packets
                         conn_ip = conn.raddr.ip
-
-                        # Port 80 is anydesk server
                         if conn.raddr.port != 80:
-
-                            # Check if is the local ip LOL
                             if not conn_ip.startswith('192.168.'):
-
-                                # Checking if the ip is duplicated
                                 if not conn_ip in ips:
-
-                                    # Adding ip to the ips list
-                                    ips.append(f"{conn_ip}")
+                                    ips.append(conn_ip)
         except psutil.NoSuchProcess:
             pass
 
     return ips
 
 def get_ip_info(conn_ip: str) -> dict:
-    """ Get information about an IP """
-
-    info_obj = get(f'http://ip-api.com/json/{conn_ip}').json()
-
-    country = info_obj.get('country', 'Unknown')
-    region = info_obj.get('regionName', 'Unknown')
-    city = info_obj.get('city', 'Unknown')
-    isp = info_obj.get('isp', 'Unknown')
-
+    j = requests.get(f'http://ip-api.com/json/{conn_ip}').json()
     return dict(
-            IP=conn_ip,
-            Country=country,
-            Region=region,
-            City=city,
-            ISP=isp
-        )
+        IP=conn_ip,
+        Country=j.get('country', 'Unknown'),
+        Region=j.get('regionName', 'Unknown'),
+        City=j.get('city', 'Unknown'),
+        ISP=j.get('isp', 'Unknown')
+    )
 
 
 def try_exit() -> None:
@@ -75,14 +44,13 @@ def try_exit() -> None:
 
 
 def main() -> None:
-    """ Main program """
     msg = 'Anydesk is turned off or noone is trying to connect to your monitor, retry... [CTRL+C to exit]'
     while True:
         try:
             ips = get_ips()
             print(' ' * len(msg), flush=False, end='\r')
-
-            # printing ips
+               
+            # Something found
             if len(ips) > 0:
                 for conn_ip in ips:
                     print("Connection Found, infos:")
